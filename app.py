b5300c3f119ffe606f05dede374bd04fbd7ecf72
abartoha <abar.toha@gmaii.com>
2021-10-23 01:07:06 +0600

    Main Run 2

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(
-  __name__,
-  template_folder='templates',
-  static_url_path= ''
-)
+app = Flask(__name__)
+
 @app.route('/')
 def hello():
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/file/<filename>")
 def download(filename):
-  return send_from_directory('data', filename)
+    return send_from_directory('data', filename)
 
-if __name__ == "__main__":
-  app.run()
+if __name__=="__main__":
+    app.run()
