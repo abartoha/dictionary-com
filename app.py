df5cd5b9962b5f87650608d1f04664057eeffaf3
abartoha <abar.toha@gmaii.com>
2021-10-23 05:36:42 +0600

    DONE

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
 from json import dumps
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://knmvydibdgtdar:90c0de41d7aec8ad8bfb0280c721bd04664bb930ee8cfe104685acfdbb2d2921@ec2-35-174-56-18.compute-1.amazonaws.com:5432/d83kspcknrffuv?sslmode=require"
-db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
